43cba60ba464938d42cf9f3917395ac676739845
Wesley Archer <wesleyarcher@hotmail.com>
2016-01-24 17:37:25 +0000

    Updated 24/01/16

--- white-on.sh
+++ white-on.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Script to turn on the White LED's on Pi-Supply.com's Bright Pi
 # Can be used as part of a Crontab to turn on at designated days / times
-# Created by Wesley Archer AKA Raspberry Coulis - Twitter: @RaspberryCoulis | Wordpress: http://raspberrycoulis.wordpress.com
+# Created by Wesley Archer AKA Raspberry Coulis
+# Twitter: @RaspberryCoulis
+# Email: raspberrycoulis.official@gmail.com
+# Wordpress: http://raspberrycoulis.wordpress.com
 
 # This will turn the White LED's
 sudo i2cset -y 1 0x70 0x00 0x5a
