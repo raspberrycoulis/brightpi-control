161715bf21cccf0e28d637fad7eeff9c4bf40801
Wesley Archer <raspberrycoulis@users.noreply.github.com>
2016-07-18 15:52:18 +0100

    Added email and new web address

--- all-off.sh
+++ all-off.sh
@@ -3,8 +3,8 @@
 # Can be used as part of a Crontab to turn on at designated days / times
 # Created by Wesley Archer AKA Raspberry Coulis
 # Twitter: @RaspberryCoulis
-# Email: raspberrycoulis.official@gmail.com
-# Wordpress: http://raspberrycoulis.wordpress.com
+# Email: wesley@raspberrycoulis.co.uk
+# Website: https://www.raspberrycoulis.co.uk
 
 # This will turn off all LED's
 sudo i2cset -y 1 0x70 0x00 0x00
